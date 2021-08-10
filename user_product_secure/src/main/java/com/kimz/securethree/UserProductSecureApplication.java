f9ad448ba91b860720b0f3a5fb1be33fe10bd284
TechKim <TechKim@192.168.1.100>
2021-08-10 04:45:40 +0300

    Initial commit

---
+++ user_product_secure/src/main/java/com/kimz/securethree/UserProductSecureApplication.java
@@ -0,0 +1,13 @@
+package com.kimz.securethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProductSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProductSecureApplication.class, args);
+	}
+
+}
---
+++ user_product_secure/src/test/java/com/kimz/securethree/UserProductSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimz.securethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
